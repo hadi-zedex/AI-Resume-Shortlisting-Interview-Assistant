05d249e339214d24e84fb78d05df87ab02e01332
Abdul Hadi <hadihadijr7@gmail.com>
2026-03-19 04:10:50 +0530

    refractor(models): add __init__

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,1 +1,29 @@
-# TODO
+from .enums import Tier, MatchType, SkillLevel, ScoreDimension
+from .candidate import CandidateProfile, CandidateSkill, Experience, Education
+from .job import JobDescription, RequiredSkill
+from .scoring import ScoringResult, DimensionScore, SkillMatchDetail
+from .tier import TierClassification, TierThresholds, TIER_LABELS
+
+__all__ = [
+    # Enums
+    "Tier",
+    "MatchType",
+    "SkillLevel",
+    "ScoreDimension",
+    # Candidate
+    "CandidateProfile",
+    "CandidateSkill",
+    "Experience",
+    "Education",
+    # Job
+    "JobDescription",
+    "RequiredSkill",
+    # Scoring
+    "ScoringResult",
+    "DimensionScore",
+    "SkillMatchDetail",
+    # Tier
+    "TierClassification",
+    "TierThresholds",
+    "TIER_LABELS",
+]
