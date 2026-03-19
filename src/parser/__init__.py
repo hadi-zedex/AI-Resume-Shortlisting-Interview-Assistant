8a622d0c42fae91273e3957b31e475c3c95dabfc
Abdul Hadi <hadihadijr7@gmail.com>
2026-03-19 23:38:36 +0530

    refactor(parser): expose parser entrypoints via package exports

--- src/parser/__init__.py
+++ src/parser/__init__.py
@@ -1,1 +1,12 @@
-# TODO
+from .pdf_parser import pdf_parser, PDFParser
+from .resume_extractor import resume_extractor, ResumeExtractor
+from .jd_extractor import jd_extractor, JDExtractor
+
+__all__ = [
+    "pdf_parser",
+    "PDFParser",
+    "resume_extractor",
+    "ResumeExtractor",
+    "jd_extractor",
+    "JDExtractor",
+]
