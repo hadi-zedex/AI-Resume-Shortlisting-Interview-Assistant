e22204e940cf0b6212fd015f474c8122088a755b
Abdul Hadi <hadihadijr7@gmail.com>
2026-03-19 23:55:32 +0530

    refactor(scorer): expose scorer entrypoints via package exports

--- src/scorer/__init__.py
+++ src/scorer/__init__.py
@@ -1,1 +1,18 @@
-# TODO
+from .exact_match import exact_match_scorer, ExactMatchScorer
+from .semantic_similarity import semantic_similarity_scorer, SemanticSimilarityScorer
+from .achievement import achievement_scorer, AchievementScorer
+from .ownership import ownership_scorer, OwnershipScorer
+from .engine import scoring_engine, ScoringEngine
+
+__all__ = [
+    "exact_match_scorer",
+    "ExactMatchScorer",
+    "semantic_similarity_scorer",
+    "SemanticSimilarityScorer",
+    "achievement_scorer",
+    "AchievementScorer",
+    "ownership_scorer",
+    "OwnershipScorer",
+    "scoring_engine",
+    "ScoringEngine",
+]
